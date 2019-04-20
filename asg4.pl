d2683641d32adee70ef3f2cbd198f5ec7688eebb
Huzeyfe Kıran <31684166+Huzo@users.noreply.github.com>
2019-04-20 17:38:12 +0800

    Add files via upload

--- asg4.pl
+++ asg4.pl
@@ -20,3 +20,11 @@ geq(X,Y) :- lt(Y,X).
 % 1.e Rules
 max(X,Y,Z) :- Z=X, geq(X,Y).
 max(X,Y,Z) :- Z=Y, geq(Y,X).
+
+% 1.f Rules
+difference(X,Y,Z) :- sum(Z,Y,X).
+
+% 1.g Rules
+mod(X,Y,Z) :- lt(Z,Y), difference(X,Z,D), multiple(Y,D).
+multiple(X,0).
+multiple(X,Y) :- lt(0,X), lt(0,Y), difference(Y,X,D), multiple(X,D).
