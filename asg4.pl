7ae0ef2852dcaafa68e63f3c4b5d36181e26e633
Huzeyfe Kıran <31684166+Huzo@users.noreply.github.com>
2019-04-22 13:52:56 +0800

    Add files via upload

--- asg4.pl
+++ asg4.pl
@@ -33,7 +33,14 @@ multiple(X,Y) :- X=s(_), Y=s(_), geq(Y,X), difference(Y,X,D), multiple(X,D).
 % bt(a,bt(nil,b,d),bt(c,bt(f,e,g),nil).
 
 % 2.b Rules
+isTree(nil).
+isTree(bt(_E,nil,nil)).
+isTree(bt(_E,_L,nil)).
+isTree(bt(_E,nil,_R)).
+isTree(bt(_E,L,R)) :- isTree(L), isTree(R).
+
+% 2.c Rules
 btMember(E,bt(E,_L,_R)).
 btMember(E,bt(_Rt,L,_R)) :- btMember(E,L).
 btMember(E,bt(_Rt,_L,R)) :- btMember(E,R).
-isTree(Term) :- btMember(Term, bt(_,_,_)).
+isLeaf(X,bt(E,L,R)) :- btMember(X,bt(E,L,R)), btMember(_A,X)=false.
