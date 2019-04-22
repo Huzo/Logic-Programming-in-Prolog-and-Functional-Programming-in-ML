3d585f0666534ea504936b04006ed8b44e996a6f
Huzeyfe Kıran <31684166+Huzo@users.noreply.github.com>
2019-04-22 22:36:09 +0800

    Add files via upload

--- asg4.ml
+++ asg4.ml
@@ -9,7 +9,19 @@ fun abs x = if x >= 0 then x else ~x;
 
 fun cardColor(x:suit,y:rank):color = if x=Spades orelse x=Clubs then Black else Red;
 
-fun cardValue(x:suit,y:rank):int = if y=Ace then 11 else 10;
+fun cardValue(_,Jack) = 10
+|   cardValue(_,Queen) = 10
+|   cardValue(_,Ace) = 11
+|   cardValue(_,King) = 10
+|   cardValue(_,Num i) = i;
 
 fun numOfCard(x:(suit*rank) list):int = length x;
 
+fun removeCard(l:(suit*rank) list, s:suit, r:rank):(suit*rank) list =
+        if (s,r) = hd(l) then tl(l)
+        else [hd(l)]@removeCard(tl(l),s,r);
+
+fun sumCards(l:(suit*rank) list, c:color):int =
+        if null(l) then 0
+        else if cardColor(hd(l)) = c then cardValue(hd(l)) + sumCards(tl(l),c)
+        else sumCards(tl(l),c);
